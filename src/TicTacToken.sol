a37b02543d2fa9ccf2a6d582499b58560697ed56
Connor Mendenhall <cmendenhall@8thlight.com>
2022-04-15 15:14:55 -0400

    Additional validations, check for wins

--- src/TicTacToken.sol
+++ src/TicTacToken.sol
@@ -2,22 +2,110 @@
 pragma solidity 0.8.10;
 
 contract TicTacToken {
+    uint256 public constant EMPTY = 0;
+    uint256 public constant X = 1;
+    uint256 public constant O = 2;
+    uint256 internal prevMove;
 
-  uint256 constant public X = 1;
-  uint256 constant public O = 2;
+    uint256[9] public board;
 
-  uint256[9] public board;
-  
-  function getBoard() public view returns (uint256[9] memory) {
-    return board;
-  }
+    function getBoard() public view returns (uint256[9] memory) {
+        return board;
+    }
 
-  function markSpace(uint256 space, uint256 marker) public {
-    require(_validMarker(marker), "Invalid Marker");
-    board[space] = marker;
-  }
+    // possible?
+    function _rowWin(uint256 row) internal view returns (uint256) {
+        uint256 column = row * 3;
+        /* 
+    
+    Previous version:
 
-  function _validMarker(uint256 marker) internal pure returns (bool) {
-      return marker == X || marker == O;
-  }
+    if ((board[column] + board[column + 1] + board[column + 2]) % 3 == 0) {
+      return board[column];
+    }
+
+    However, this returns a false positive for some rows that add up to 3 but contain
+    mixed symbols. For example:
+
+    [0, 1, 2]
+    [2, 0, 1]
+    [1, 2, 0]
+    etc...  
+    
+    */
+        uint256 product = board[column] * board[column + 1] * board[column + 2];
+        if (product == 8) {
+            return O;
+        }
+        if (product == 1) {
+            return X;
+        }
+        return 0;
+    }
+
+    function _colWin(uint256 col) internal view returns (uint256) {}
+
+    function _diagWin() internal view returns (uint256) {
+        uint256 product = board[0] * board[4] * board[8];
+        if (product == 8) {
+            return O;
+        }
+        if (product == 1) {
+            return X;
+        }
+        return 0;
+    }
+
+    function _antiDiagWin() internal view returns (uint256) {
+        uint256 product = board[2] * board[4] * board[6];
+        if (product == 8) {
+            return O;
+        }
+        if (product == 1) {
+            return X;
+        }
+        return 0;
+    }
+
+    function winner() public view returns (uint256) {
+        uint256[5] memory potentialWins = [
+            _rowWin(0),
+            _rowWin(1),
+            _rowWin(2),
+            _diagWin(),
+            _antiDiagWin()
+        ];
+        for (uint256 i; i < potentialWins.length; i++) {
+            if (potentialWins[i] != 0) {
+                return potentialWins[i];
+            }
+        }
+        return 0;
+    }
+
+    function markSpace(uint256 space, uint256 marker) public {
+        require(_validSpace(space), "Invalid space");
+        require(_validMarker(marker), "Invalid Marker");
+        require(_emptySpace(space), "Space already occupied");
+        require(_validTurn(marker), "Turns should alternate between X and O");
+
+        board[space] = marker;
+        prevMove = marker;
+    }
+
+    function _validSpace(uint256 space) internal pure returns (bool) {
+        return space < 9;
+    }
+
+    function _validMarker(uint256 marker) internal pure returns (bool) {
+        return marker == X || marker == O;
+    }
+
+    function _emptySpace(uint256 space) internal view returns (bool) {
+        return board[space] == EMPTY;
+    }
+
+    function _validTurn(uint256 nextMove) internal view returns (bool) {
+        return nextMove != prevMove;
+    }
 }
--- src/test/TicTacToken.t.sol
+++ src/test/TicTacToken.t.sol
@@ -6,22 +6,21 @@ import "forge-std/Vm.sol";
 import "../TicTacToken.sol";
 
 contract TestTicTacToken is DSTest {
-
     uint256 constant EMPTY = 0;
-    uint256 constant public X = 1;
-    uint256 constant public O = 2;
+    uint256 public constant X = 1;
+    uint256 public constant O = 2;
 
     Vm public constant vm = Vm(HEVM_ADDRESS);
     TicTacToken internal ttt;
 
     function setUp() public {
         ttt = new TicTacToken();
-    } 
+    }
 
     function test_first_square_of_board_is_empty() public {
         assertEq(ttt.board(0), EMPTY);
     }
-    
+
     function test_last_square_of_board_is_empty() public {
         assertEq(ttt.board(8), EMPTY);
     }
@@ -36,7 +35,7 @@ contract TestTicTacToken is DSTest {
         uint256[9] memory board = ttt.getBoard();
         for (uint256 i; i < 9; i++) {
             assertEq(board[i], EMPTY);
-        } 
+        }
     }
 
     function test_marks_first_square_with_x() public {
@@ -44,7 +43,7 @@ contract TestTicTacToken is DSTest {
         uint256[9] memory board = ttt.getBoard();
         assertEq(board[0], X);
     }
-    
+
     function test_marks_first_square_with_o() public {
         ttt.markSpace(0, O);
         uint256[9] memory board = ttt.getBoard();
@@ -54,8 +53,107 @@ contract TestTicTacToken is DSTest {
     function test_validates_marker_is_x_or_o() public {
         ttt.markSpace(0, X);
         ttt.markSpace(1, O);
-        
+
         vm.expectRevert("Invalid Marker");
         ttt.markSpace(3, 3);
+    }
+
+    function test_cannot_overwrite_marked_square() public {
+        ttt.markSpace(2, X);
+
+        vm.expectRevert("Space already occupied");
+        ttt.markSpace(2, O);
+    }
+
+    function test_validates_marker_is_valid_index() public {
+        vm.expectRevert("Invalid space");
+        ttt.markSpace(9, X);
+    }
+
+    function test_validates_alternating_turns_with_x() public {
+        ttt.markSpace(0, X);
+
+        vm.expectRevert("Turns should alternate between X and O");
+        ttt.markSpace(1, X);
+    }
+
+    function test_validates_alternating_turns_with_o() public {
+        ttt.markSpace(0, O);
+
+        vm.expectRevert("Turns should alternate between X and O");
+        ttt.markSpace(1, O);
+    }
+
+    function test_checks_diagonal_win() public {
+        ttt.markSpace(0, X);
+        ttt.markSpace(1, O);
+        ttt.markSpace(4, X);
+        ttt.markSpace(2, O);
+        ttt.markSpace(8, X);
+
+        assertEq(ttt.winner(), X);
+    }
+
+    function test_checks_antidiagonal_win() public {
+        ttt.markSpace(6, X);
+        ttt.markSpace(1, O);
+        ttt.markSpace(4, X);
+        ttt.markSpace(3, O);
+        ttt.markSpace(2, X);
+
+        assertEq(ttt.winner(), X);
+    }
+
+    function xtest_checks_column_win() public {
+        ttt.markSpace(0, O);
+        ttt.markSpace(1, X);
+        ttt.markSpace(3, O);
+        ttt.markSpace(2, X);
+        ttt.markSpace(6, O);
+
+        assertEq(ttt.winner(), O);
+    }
+
+    function test_checks_row_win() public {
+        ttt.markSpace(0, X);
+        ttt.markSpace(3, O);
+        ttt.markSpace(1, X);
+        ttt.markSpace(4, O);
+        ttt.markSpace(2, X);
+
+        assertEq(ttt.winner(), X);
+    }
+
+    function test_checks_row_win2() public {
+        ttt.markSpace(0, O);
+        ttt.markSpace(3, X);
+        ttt.markSpace(1, O);
+        ttt.markSpace(4, X);
+        ttt.markSpace(2, O);
+
+        assertEq(ttt.winner(), O);
+    }
+
+    function test_checks_row_win3() public {
+        ttt.markSpace(6, O);
+        ttt.markSpace(3, X);
+        ttt.markSpace(7, O);
+        ttt.markSpace(4, X);
+        ttt.markSpace(8, O);
+
+        assertEq(ttt.winner(), O);
+    }
+
+    function test_checks_tie() public {
+        ttt.markSpace(0, X);
+        ttt.markSpace(1, O);
+        ttt.markSpace(2, X);
+        ttt.markSpace(3, O);
+        ttt.markSpace(4, X);
+        ttt.markSpace(5, O);
+        ttt.markSpace(7, X);
+        ttt.markSpace(6, O);
+
+        // assertEq(ttt.winner(), 0);
     }
 }
